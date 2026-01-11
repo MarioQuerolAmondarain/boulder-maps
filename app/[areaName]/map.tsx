03481fb4ce77196a5913e3d553ca12a7b9c27fd0
Mario Querol <marioqueroamondarain@gmail.com>
2026-01-11 16:40:09 +0100

    Config eas

--- app/[areaName]/map.tsx
+++ app/[areaName]/map.tsx
@@ -1,6 +1,11 @@
+import Mapbox, { Camera, MapView } from "@rnmapbox/maps";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import React, { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+Mapbox.setAccessToken(
+  "pk.eyJ1IjoibXF1ZXJvbCIsImEiOiJjbWs5bmJpZ2gxamVlM2NyNW44MWs4b2J4In0.T99Z7_RmAgog1D-5Xxzj6w"
+);
 
 export default function AreaMap() {
   const navigation = useNavigation();
@@ -14,5 +19,34 @@ export default function AreaMap() {
     });
   }, [navigation, areaName]);
 
-  return <Text>{areaName}</Text>;
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapView style={styles.map}>
+          <Camera
+            defaultSettings={{
+              centerCoordinate: [43.352926, -3.006046],
+            }}
+          />
+        </MapView>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  container: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "tomato",
+  },
+  map: {
+    flex: 1,
+  },
+});
