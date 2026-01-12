b7792f8ae7d2d72936e8711a88ca92988d99ac3f
Mario Querol <marioqueroamondarain@gmail.com>
2026-01-12 21:29:52 +0100

    Ajustes del mapa para centrar la camara, mostrar la ubicacion y un punto

--- app/[areaName]/map.tsx
+++ app/[areaName]/map.tsx
@@ -1,15 +1,28 @@
-import Mapbox, { Camera, MapView } from "@rnmapbox/maps";
+import Mapbox, {
+  Camera,
+  Images,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from "@rnmapbox/maps";
+import { featureCollection, point } from "@turf/helpers";
+import * as Location from "expo-location";
 import { useLocalSearchParams, useNavigation } from "expo-router";
-import React, { useLayoutEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useLayoutEffect } from "react";
+import { StyleSheet } from "react-native";
 
-Mapbox.setAccessToken(
-  "pk.eyJ1IjoibXF1ZXJvbCIsImEiOiJjbWs5bmJpZ2gxamVlM2NyNW44MWs4b2J4In0.T99Z7_RmAgog1D-5Xxzj6w"
-);
+Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN || "");
 
 export default function AreaMap() {
   const navigation = useNavigation();
   const { areaName } = useLocalSearchParams();
+
+  useEffect(() => {
+    (async () => {
+      await Location.requestForegroundPermissionsAsync();
+    })();
+  }, []);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -20,17 +33,33 @@ export default function AreaMap() {
   }, [navigation, areaName]);
 
   return (
-    <View style={styles.page}>
-      <View style={styles.container}>
-        <MapView style={styles.map}>
-          <Camera
-            defaultSettings={{
-              centerCoordinate: [43.352926, -3.006046],
-            }}
-          />
-        </MapView>
-      </View>
-    </View>
+    <MapView style={styles.map} styleURL="mapbox://styles/mapbox/outdoors-v12">
+      <Camera
+        zoomLevel={13}
+        animationDuration={0}
+        centerCoordinate={[-2.992666, 43.34254]}
+      />
+      <LocationPuck puckBearingEnabled puckBearing="heading" />
+
+      <ShapeSource
+        id="boulders"
+        shape={featureCollection([point([-2.992666, 43.34254])])}
+      >
+        <SymbolLayer
+          id="boulder-icons"
+          style={{
+            iconImage: "pin",
+            iconSize: 0.5,
+          }}
+        />
+
+        <Images
+          images={{
+            pin: require("../../assets/images/pin.png"),
+          }}
+        />
+      </ShapeSource>
+    </MapView>
   );
 }
 
