770884dfc1d6724c4ae2c5a86f22aecfee6c601e
Mario Querol <marioqueroamondarain@gmail.com>
2026-02-08 19:42:59 +0100

    Se muestra png para los parking points

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,4 +1,3 @@
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
 
@@ -44,8 +43,4 @@ export const ShareIcon = (props: any) => (
 
 export const HeartIcon = (props: any) => (
   <Ionicons name="heart-outline" size={30} color="black" {...props} />
-);
-
-export const ParkingIcon = (props: any) => (
-  <FontAwesome5 name="parking" size={24} color="#1a3a52" {...props} />
 );
--- components/ParkingMarkers.tsx
+++ components/ParkingMarkers.tsx
@@ -1,5 +1,5 @@
 import { PARKING_POINTS } from "@/data/parkingPoints";
-import { CircleLayer, ShapeSource, SymbolLayer } from "@rnmapbox/maps";
+import { Images, ShapeSource, SymbolLayer } from "@rnmapbox/maps";
 import React, { useMemo, useState } from "react";
 import ParkingModal from "./ParkingModal";
 
@@ -39,27 +39,20 @@ export default function ParkingMarkers({ areaId }: { areaId?: number }) {
     return null;
   }
 
-  // TODO Cambiar icono por ParkingIcon
   return (
     <>
       <ShapeSource id="parking-points" shape={shape} onPress={onPointPress}>
-        <CircleLayer
-          id="parking-circle"
-          minZoomLevel={PARKING_MIN_ZOOM}
-          style={{
-            circleColor: "#3b82f6",
-            circleRadius: 9,
-            circleStrokeWidth: 2,
-            circleStrokeColor: "white",
-          }}
-        />
         <SymbolLayer
           id="parking-icon"
           minZoomLevel={PARKING_MIN_ZOOM}
           style={{
-            iconImage: "parking-icon",
-            iconSize: 0.9,
+            iconImage: "icon",
             iconAllowOverlap: true,
+          }}
+        />
+        <Images
+          images={{
+            icon: require("../assets/images/parking-icon.png"),
           }}
         />
       </ShapeSource>
